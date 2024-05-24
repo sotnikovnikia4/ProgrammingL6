a4df413025fd88e17cdf3c1b08e74f2c63a88ba9
sotnikovnikia4 <sotnikovnikita4@gmail.com>
2024-05-24 10:00:59 +0300

    попытка 1

--- ProgrammingL6VS/task17.cpp
+++ ProgrammingL6VS/task17.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <bitset>
 
 #include "constants.h"
 #include "myfunctions.h"
@@ -10,30 +11,48 @@ using namespace constants;
 using ullong = unsigned long long;
 
 bool argsAreCorrect17(int countArgs, char* args[]) {
-	if (countArgs < 5) {
-		cout << "Введено мало аргументов";
+	try {
+		if (countArgs < 5) {
+			cout << "Введено мало аргументов";
+			return false;
+		}
+		else if (!isullong(args[2]) || !isullong(args[3]) || !isullong(args[4])) {
+			cout << "Аргументы должны быть типа ullong" << endl;
+			return false;
+		}
+		else if (stoull(args[3]) > sizeLongLong) {
+			cout << "Позиция должна быть между 1 и " << sizeLongLong << endl;
+			return false;
+		}
+		else if (stoull(args[4]) >= (1 << 8)) {
+			cout << "Записываемый байт действительно должен быть байтом (<256)" << endl;
+			return false;
+		}
+		else
+			return true;
+	}
+	/*catch (std::invalid_argument& e) {
+		cout << "Введенный(е) аргумент(ы) выходят за пределы типа ullong" << endl;
 		return false;
 	}
-	else if (!isullong(args[2]) || !isullong(args[3]) || !isullong(args[4])) {
-		cout << "Аргументы должны быть типа ullong" << endl;
+	catch (std::out_of_range& y) {
+		cout << "Введенный(е) аргумент(ы) выходят за пределы типа ullong" << endl;
 		return false;
+	}*/
+	catch (out_of_range& у) {
+		cout << "Число выходит за пределы" << endl;
+		exit(0);
 	}
-	else if (stoull(args[3]) > sizeLongLong) {
-		cout << "Позиция должна быть между 1 и " << sizeLongLong << endl;
-		return false;
+	catch (invalid_argument& e) {
+		cout << "Число выходит за пределы" << endl;
+		exit(0);
 	}
-	else if (stoull(args[4]) >= (1 << 8)) {
-		cout << "Записываемый байт действительно должен быть байтом (<256)" << endl;
-		return false;
-	}
-	else
-		return true;
 }
 
 void printInputArgs(ullong number, ullong pos, ullong byte) {
-	cout << "Исходное число: " << number << endl;
+	cout << "Исходное число: " << bitset<sizeLongLong>(number) << endl;
 	cout << "Позиция: " << pos << endl;
-	cout << "Байт, который нужно записать: " << byte << endl << endl;
+	cout << "Байт, который нужно записать: " << bitset<8>(byte) << endl << endl;
 }
 
 ullong getModifiedNumber(ullong number, ullong pos, ullong byte) {
@@ -73,5 +92,5 @@ void doTask17(int countArgs, char* args[]) {
 
 	number = getModifiedNumber(number, pos, byte);
 	
-	cout << "Преобразованное число: " << number << endl << endl;
+	cout << "Преобразованное число: " << bitset<sizeLongLong>(number) << endl << endl;
 }
